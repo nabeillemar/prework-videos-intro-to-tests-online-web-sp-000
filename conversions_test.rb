243b1017d1df4e9a5be6cabbd9302bfe8ca7da4c
Nabeil Lemar <nabeillemar@gmail.com>
2020-06-27 04:43:24 +0000

    Automatically backed up by Learn

--- conversions_test.rb
+++ conversions_test.rb
@@ -15,3 +15,11 @@ if answer == 28.3495
 else
   puts "Test failed: got #{answer} instead"
 end
+
+puts "Given 1, it should return 28.3495"
+answer = ounces_to_grams(1)
+if answer == 28.3495
+  puts "Passed!"
+else
+  puts "Test failed: got #{answer} instead"
+end
